5895234a85d76512c1437619e1a2c295364927b7
asthara10 <mirp.julia@gmail.com>
2018-09-03 17:30:46 +0200

    pandas syntax errors solved

--- createCutsR.py
+++ createCutsR.py
@@ -2,36 +2,36 @@ def GetData(df, cut):
 	"""Retrieves the start and end data of each cut in the data frame.
 	The cut variable corresponds at the number of the nucleotide that will be left before the cut."""
 	import pandas
-	data = [df.loc[:,'seqnames'], df.loc[:,'start'], df.loc[:,'end'], df.loc[:,'strand']]
 	i = 0
 	cuts = []
 	newcuts = []
 	first = True
 	
-	while i < len(data[0]):
+	while i < len(df.loc[:,'seqnames']):
 		if first:
-			chr = data[0][i]
-			if data[3][i] == "+":
-				newcuts.append(data[1][i] + cut-1)
+			chr = df.loc[i,'seqnames']
+			if df.loc[i,'strand'] == "+":
+				newcuts.append(df.loc[i,'start'] + cut-1)
 			else:
-				newcuts.append(data[2][i] - cut-1)
+				newcuts.append(df.loc[i,'end'] - cut-1)
 			first = False
 		else:
-			newchr = data[0][i]
+			newchr = df.loc[i,'seqnames']
 			if newchr == chr:
-				if data[3][i] == "+":
-					newcuts.append(data[1][i] + cut-1)
+				if df.loc[i,'strand'] == "+":
+					newcuts.append(df.loc[i,'start'] + cut-1)
 				else:
-					newcuts.append(data[2][i] - cut-1)
+					newcuts.append(df.loc[i,'end'] - cut-1)
 			else:
 				cuts.append(newcuts)
 				newcuts = []
 				chr = newchr
-				if data[3][i] == "+":
-					newcuts.append(data[1][i] + cut-1)
+				if df.loc[i,'strand'] == "+":
+					newcuts.append(df.loc[i,'start'] + cut-1)
 				else:
-					newcuts.append(data[2][i] - cut-1)
+					newcuts.append(df.loc[i,'end'] - cut-1)
 		cuts.append(newcuts)
+		i+=1
 
 	return cuts
 	
