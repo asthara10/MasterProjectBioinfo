8ecf816f1e15ed2ef77f514c8eaf8ab3ab13121e
asthara10 <mirp.julia@gmail.com>
2018-09-05 13:55:21 +0200

    problem with Ns in genome solved

--- DigestGenome.R
+++ DigestGenome.R
@@ -12,38 +12,108 @@ print("genome retrieved")
 
 ### Saving enzymes
 
-Cas9F <- DNAString("GGTGACCCTCCTCCAGTACGNGG")
-Cas9R <- DNAString("GGTCATCCACGTACTGGAGGNGG")
+Cas9FA <- DNAString("GGTGACCCTCCTCCAGTACGAGG")
+Cas9FC <- DNAString("GGTGACCCTCCTCCAGTACGCGG")
+Cas9FT <- DNAString("GGTGACCCTCCTCCAGTACGTGG")
+Cas9FG <- DNAString("GGTGACCCTCCTCCAGTACGGGG")
+Cas9RA <- DNAString("GGTCATCCACGTACTGGAGGAGG")
+Cas9RC <- DNAString("GGTCATCCACGTACTGGAGGCGG")
+Cas9RT <- DNAString("GGTCATCCACGTACTGGAGGTGG")
+Cas9RG <- DNAString("GGTCATCCACGTACTGGAGGGGG")
 
 ### Obtain cuts
 
-CutsCas9F <- vmatchPattern(Cas9F, genome, fixed=FALSE)
-CutsCas9R <- vmatchPattern(Cas9R, genome, fixed=FALSE)
+CutsCas9FA <- vmatchPattern(Cas9FA, genome)
+CutsCas9FC <- vmatchPattern(Cas9FC, genome)
+CutsCas9FT <- vmatchPattern(Cas9FT, genome)
+CutsCas9FG <- vmatchPattern(Cas9FG, genome)
+CutsCas9RA <- vmatchPattern(Cas9RA, genome)
+CutsCas9RC <- vmatchPattern(Cas9RC, genome)
+CutsCas9RT <- vmatchPattern(Cas9RT, genome)
+CutsCas9RG <- vmatchPattern(Cas9RG, genome)
 
 ### Save as data frame
 
-RangesF = as(CutsCas9F, "data.frame")
-RangesR = as(CutsCas9R, "data.frame")
+RangesFA = as(CutsCas9FA, "data.frame")
+RangesFC = as(CutsCas9FC, "data.frame")
+RangesFT = as(CutsCas9FT, "data.frame")
+RangesFG = as(CutsCas9FG, "data.frame")
+RangesRA = as(CutsCas9RA, "data.frame")
+RangesRC = as(CutsCas9RC, "data.frame")
+RangesRT = as(CutsCas9RT, "data.frame")
+RangesRG = as(CutsCas9RG, "data.frame")
 print("patterns found")
-print(nrow(RangesF))
-print(nrow(RangesR))
+for (chr in names(genome)) {
+  if (! chr %in% RangesFA$seqnames) {
+    add <- data.frame("seqnames"=chr, "start"=0, "end"=0, "width"=0, "strand"="+")
+    RangesFA <- rbind(RangesFA, add)
+  }
+}
+for (chr in names(genome)) {
+  if (! chr %in% RangesFC$seqnames) {
+    add <- data.frame("seqnames"=chr, "start"=0, "end"=0, "width"=0, "strand"="+")
+    RangesFC <- rbind(RangesFC, add)
+  }
+}
+for (chr in names(genome)) {
+  if (! chr %in% RangesFT$seqnames) {
+    add <- data.frame("seqnames"=chr, "start"=0, "end"=0, "width"=0, "strand"="+")
+    RangesFT <- rbind(RangesFT, add)
+  }
+}
+for (chr in names(genome)) {
+  if (! chr %in% RangesFG$seqnames) {
+    add <- data.frame("seqnames"=chr, "start"=0, "end"=0, "width"=0, "strand"="+")
+    RangesFG <- rbind(RangesFG, add)
+  }
+}
+for (chr in names(genome)) {
+  if (! chr %in% RangesRA$seqnames) {
+    add <- data.frame("seqnames"=chr, "start"=0, "end"=0, "width"=0, "strand"="+")
+    RangesRA <- rbind(RangesRA, add)
+  }
+}
+for (chr in names(genome)) {
+  if (! chr %in% RangesRC$seqnames) {
+    add <- data.frame("seqnames"=chr, "start"=0, "end"=0, "width"=0, "strand"="+")
+    RangesRC <- rbind(RangesRC, add)
+  }
+}
+for (chr in names(genome)) {
+  if (! chr %in% RangesRT$seqnames) {
+    add <- data.frame("seqnames"=chr, "start"=0, "end"=0, "width"=0, "strand"="+")
+    RangesRT <- rbind(RangesRT, add)
+  }
+}
+for (chr in names(genome)) {
+  if (! chr %in% RangesRG$seqnames) {
+    add <- data.frame("seqnames"=chr, "start"=0, "end"=0, "width"=0, "strand"="+")
+    RangesRG <- rbind(RangesRG, add)
+  }
+}
 
 ### Run python script to obtain cuts
 
 cutCas9 = 17
 source_python('./createCutsR.py')
 print("python imported")
-listF <- GetData(RangesF, cutCas9)
+listFA <- GetData(RangesFA, cutCas9)
+listFC <- GetData(RangesFC, cutCas9)
+listFT <- GetData(RangesFT, cutCas9)
+listFG <- GetData(RangesFG, cutCas9)
 print("GetDataF completed")
-listR <- GetData(RangesR, cutCas9)
+listRA <- GetData(RangesRA, cutCas9)
+listRC <- GetData(RangesRC, cutCas9)
+listRT <- GetData(RangesRT, cutCas9)
+listRG <- GetData(RangesRG, cutCas9)
 print("GetDataR completed")
-allStarts = ConvineCuts(list(listF, listR))
+allStarts = ConvineCuts(list(listFA, listFC, listFT, listFG, listRA, listRC, listRT, listRG))
 print("all python functions finished")
 
 newGenome = DNAStringSet()
 i <- 1
 print("starting while loop")
-while (i <= length(genome)){
+while (i <= length(allStarts)){
   j <- 1
   for (nt in allStarts[[i]]){
     if (j < length(allStarts[[i]])) {
--- createCutsR.py
+++ createCutsR.py
@@ -2,17 +2,16 @@ def GetData(df, cut):
 	"""Retrieves the start and end data of each cut in the data frame.
 	The cut variable corresponds at the number of the nucleotide that will be left before the cut."""
 	import pandas
-	i = 0
 	cuts = []
 	newcuts = []
 	first = True
 	
-	while i < len(df.loc[:,'seqnames']):
+	for i in range(len(df.loc[:,'seqnames'])):
 		if first:
 			chr = df.loc[i,'seqnames']
-			if df.loc[i,'strand'] == "+":
+			if (df.loc[i,'strand'] == "+") and (df.loc[i,'start'] != 0):
 				newcuts.append(df.loc[i,'start'] + cut-1)
-			else:
+			elif (df.loc[i,'strand'] == "-"):
 				newcuts.append(df.loc[i,'end'] - cut-1)
 			first = False
 		else:
@@ -26,11 +25,10 @@ def GetData(df, cut):
 				cuts.append(newcuts)
 				newcuts = []
 				chr = newchr
-				if df.loc[i,'strand'] == "+":
+				if (df.loc[i,'strand'] == "+") and (df.loc[i,'start'] != 0):
 					newcuts.append(df.loc[i,'start'] + cut-1)
-				else:
+				elif (df.loc[i,'strand'] == "-"):
 					newcuts.append(df.loc[i,'end'] - cut-1)
-		i+=1
 	cuts.append(newcuts)
 
 	return cuts
@@ -40,15 +38,13 @@ def ConvineCuts(LoL):
 	Argument LoL is the List of Lists of the multiple enzymes cuts.
 	Sorts from smaller to larger."""
 	final = []
-	i = 0
 	
-	while i < len(LoL[0]):
+	for i in range(len(LoL[0])):
 		new = []
 		for L in LoL:
 			new = new + L[i]
 		new.append(0)
 		new.sort()
 		final.append(new)
-		i+=1
 
 	return final
